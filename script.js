662594a3653bf279b2fca8900e9cf59e1d4c368b
Christopher Clan <chrisclan17@gmail.com>
2021-06-18 22:30:20 -0400

    changed code popups to use gist from github

--- script.js
+++ script.js
@@ -5,14 +5,14 @@
 
 // sticky nav on window scroll 
 var navbar = document.querySelector("nav");
-window.addEventListener("scroll", function(){ 
-  const pixels = window.pageYOffset;
-  if (pixels > window.innerHeight - 50) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-    
-  }
+window.addEventListener("scroll", function () {
+    const pixels = window.pageYOffset;
+    if (pixels > window.innerHeight - 50) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+
+    }
 });
 
 
@@ -29,34 +29,34 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         console.log('this.getAttributehref = ' + this.getAttribute('href'));
     });
 });
-    
-    
+
+
 //add animate for each section 
 const config = {
     root: null,
     rootMargin: '0px',
     threshold: 0.3
-  };
-  window.addEventListener('DOMContentLoaded', () => {
-    const observer = new IntersectionObserver(function(entries) {
+};
+window.addEventListener('DOMContentLoaded', () => {
+    const observer = new IntersectionObserver(function (entries) {
         entries.forEach(entry => {
             const id = entry.target.getAttribute('id');
             if (entry.isIntersecting) {
                 document.querySelector(`section[id="${id}"]`).classList.add('fade');
                 document.querySelector(`section[id="${id}"]`).classList.add('animate');
                 console.log("adding animate to " + id)
-            } 
+            }
             console.log("not adding")
         });
     }, config);
-  
+
     // Track all sections that have an `id` applied
     document.querySelectorAll('section[id]').forEach((section) => {
         observer.observe(section);
     });
-    
-  }); 
-    
+
+});
+
 
 
 //Code Modal
@@ -70,31 +70,53 @@ var span = document.getElementsByClassName("close")[0];
 var codeTitle = document.querySelector(".codeTitle");
 
 
-for (var i = 0; i < codeButtons.length; i++) {
-    var cBtn = codeButtons[i];   
-    // When the button is clicked...
-    cBtn.onclick = function(event) {
-        currFileId = this.id;
-        currFileTitle = this.title;
+// for (var i = 0; i < codeButtons.length; i++) {
+//     var cBtn = codeButtons[i];
+//     // When the button is clicked...
+//     cBtn.onclick = function (event) {
+//         currFileId = this.id;
+//         currFileTitle = this.title;
+//         currFileAlt = this.alt;
 
-        console.log(currFileId + " is the id");
-        console.log(currFileTitle + " is the title");
+//         console.log(currFileId + " is the id");
+//         console.log(currFileTitle + " is the title");
+//         console.log(currFileAlt + " is the alt");
 
-        currentTextFile.innerHTML = '<iframe src="' + currFileId + '" marginwidth="0" marginheight="0"></iframe>';
-        modal.style.display = 'block';
-        
-    };
+//         // currentTextFile.innerHTML = '<script src="' + currFileId + '"></script><link rel="stylesheet" href="https://github.githubassets.com/assets/gist-embed-' + currFileTitle + '.css">';
+//         modal.style.display = 'block';
+
+//     };
+// }
+
+function displayCode(codeId) {
+    modal.style.display = 'block';
+    codeId.style.display = 'block';
+    document.body.style.overflow = 'hidden';
 }
 
-span.onclick = function() {
+span.onclick = function () {
     modal.style.display = "none";
+    document.body.style.overflow = 'scroll';
+    for (var i = 0; i < modalScripts.length; i++) {
+        modalScripts[i].style.display = "none"; // depending on what you're doing
+    }
 }
 
-
+var modalScripts = document.getElementsByClassName("modalScripts");
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+        document.body.style.overflow = 'scroll';
+        for (var i = 0; i < modalScripts.length; i++) {
+            modalScripts[i].style.display = "none"; // depending on what you're doing
+        }
+    }
 }
+
+// var divsToHide = document.getElementsByClassName("classname"); //divsToHide is an array
+//     for(var i = 0; i < modalScripts.length; i++){
+//         modalScripts[i].style.visibility = "hidden"; // or
+//         modalScripts[i].style.display = "none"; // depending on what you're doing
+//     }
